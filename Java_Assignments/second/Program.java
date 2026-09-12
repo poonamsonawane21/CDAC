45218ed35311407bda720c23edc3d183ed9c472e
poonamsonawane21 <poonam.sonawane.cmaug26@gmail.com>
2026-09-12 11:57:36 +0530

    Add files via upload

---
+++ Java_Assignments/second/Program.java
@@ -0,0 +1,21 @@
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		System.out.println(args[0]+args[1]); 	//in string concatenation
+		int num1=10;             				//initialization
+		int num2;
+		int result;
+		
+		num2=20;                 				//assignment
+		
+		result=num1+num2;
+		
+		System.out.println("Result is " + result); //integer gets add
+		
+	}
+}
+
+
+//run->run configuration->arguments->10 20->run
